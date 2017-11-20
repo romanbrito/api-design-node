dcbce66724392086cabe2e78ec124809ce7735ef
Roman Brito <romanbrito1@gmail.com>
2017-11-19 19:27:32 -0600

    more promises examples

--- promises.js
+++ promises.js
@@ -1,6 +1,47 @@
-// promises
+// promises (some libraries are bluebird or Q or the node built in
 // alternatives to promises: generators, async functions, observables
 
+
+import fs from 'fs';
+
+// more complex example
+const readfile = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./package.json', (err, file) =>{
+      return err ? reject(err) : resolve(file.toString());
+    })
+  })
+};
+
+readfile()
+  .then((file)=>{
+  console.log(file);
+  })
+  .catch((err) => {
+  console.log(err);
+  })
+  .finally(console.log('the end')); //sometimes called done
+
+// promise.all, another example
+const readAllFiles = () => {
+  const promises = [readfile(), readfile(), readfile()];
+  return Promise.all(promises);
+};
+
+const logFile = () => {
+  return readfile()
+    .then(() => {
+    return readfile()
+    })
+};
+
+// start loading
+readAllFiles()
+  .then((files) => {
+  console.log(files)
+  });
+
+// simple example
 // problem solved using callbacks
 const action = (cb) => {
   setTimeout(() => {
