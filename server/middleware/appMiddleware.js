140ccabcb97c1fb0e25d7c9d3fd66a98cb9f4604
Roman Brito <romanbrito1@gmail.com>
2017-11-22 09:56:35 -0600

    added cors middleware

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,11 +1,13 @@
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 // setup global middleware here
 
 const middleware = app => {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true}));
   app.use(bodyParser.json());
+  app.use(cors());
 };
 
 export default middleware;
